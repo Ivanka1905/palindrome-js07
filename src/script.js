80cb53047d4d668eaf173db3c204c54c2beb9333
Ivanka <IvNagornyuk@gmail.com>
2025-05-30 00:37:23 +0300

    changed function

--- src/script.js
+++ src/script.js
@@ -12,5 +12,6 @@ function isPalindrome(text) {
   const newWord = getCleanText(text);
   return newWord === getReversedText(newWord);
 }
+
 console.log(isPalindrome("A man, a plan, a canal--panama"));
 console.log(isPalindrome("abcd1234"))
